6f88adba0d6bdf8cff24a6e428c04b9ed65ec04d
Marina Vabistsevits <marina.vabi@hotmail.com>
2025-04-29 16:43:19 +0100

    add annotation scripts

---
+++ 01_annotate_GWAS_with_rsids.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Workflow for annotating GWAS with rsids from dbSNP
+
+# Get arguments 
+dbSNP_data=$1 # dbsnp ref file generated in the previous step
+raw_file=$2 # GWAS file; provide full path; assumes no dots in file name except the ones separating the extensions
+
+# Separate GWAS file basename and extension
+basename="${raw_file%%.*}"
+extension="${raw_file#$basename}" 
+
+mkdir -p tmp_files
+
+# 1) Replace chr 23 in regenie output with X, because dbSNP data contains X
+echo "---------- Preparing GWAS ----------"
+gunzip "$basename$extension" # unzip
+awk '{ if ($1 != 23) { print }
+       else if ($1 == 23) { $1 = "X"; print }
+     }' "$basename".txt > tmp_files/tmp_file
+gzip "$basename".txt # zip again
+
+# 2) Extract chr:pos present in GWAS file from the dbSNP ref file
+echo "---------- Extracting GWAS SNPs from dbSNP ref file ---------- "
+
+# create a list of chr:pos to extract
+awk 'NR > 1 { print $1, $2}' OFS=':' tmp_files/tmp_file | sort | uniq > tmp_files/chr_pos_list.txt
+
+# extract the list from ref file
+grep -Fwf tmp_files/chr_pos_list.txt  "$dbSNP_data" > tmp_files/chr_pos_list_w_rsid.txt
+
+# split chr:pos:ref:alt into four columns in bash to save computing time in R
+cat tmp_files/chr_pos_list_w_rsid.txt | cut -d $'\t' -f 1,3 --output-delimiter=$'\t' | tr ':' '\t' >  tmp_files/chr_pos_list_w_rsid_split.txt
+
+
+# 3) in R, merge rsids to GWAS file # this step is slow ~ 15 mins
+echo "---------- Running R script to annotate rsIDs ---------- "
+#module load R-bundle-Packages/4.3.2-20240227-gfbf-2023a (for use on Exeter servers only - uncomment)
+Rscript 01_annotate_helper.R tmp_files/chr_pos_list_w_rsid_split.txt "$basename$extension" "$basename"_rsids"$extension"
---
+++ 01_annotate_helper.R
@@ -0,0 +1,65 @@
+library(dplyr)
+library(vroom)
+library(readr)
+library(tidyr)
+
+# ----------------------------------------
+# Process arguments
+# ----------------------------------------
+
+args <- commandArgs(trailingOnly = TRUE)
+rsid_subset_file <- args[1] # "tmp_files/chr_pos_list_w_rsid_split.txt"
+gwas_file_in <- args[2]
+print(paste0("Input GWAS: ",gwas_file_in))
+gwas_file_out <- args[3]
+print(paste0("Output GWAS: ",gwas_file_out))
+
+# ----------------------------------------
+# Read and tidy data (GWAS and annotation)
+# ----------------------------------------
+
+print("Reading and processing GWAS data ..")
+gwas_data <- vroom::vroom(gwas_file_in,
+    col_select = c(ID, CHROM, GENPOS, A1FREQ, ALLELE0, ALLELE1, BETA, SE, LOG10P)) %>%
+  dplyr::rename("CHR" = "CHROM", "POS" = "GENPOS") %>%
+  dplyr::mutate(P = 10^-(LOG10P)) %>%
+  dplyr::mutate(CHR = ifelse(CHR == 23, "X", CHR)) %>%
+  dplyr::mutate(chr_pos = paste0(CHR, ":", POS))
+
+print("Reading and processing rsID annotation data ..")
+annot <- vroom::vroom(rsid_subset_file, col_names = c("SNP", "CHR", "POS", "ALLELE0", "ALLELE1"))
+# tidy up multiallelic varaints (they are listed in a signle row e.g A:TA,TAA,TAAA - this will split them into multiple rows)
+annot <- annot %>% tidyr::separate_rows(ALLELE1, sep = ",") 
+
+# ----------------------------------------
+# Merging annotation data
+# ----------------------------------------
+
+print("Merging annotation file ..")
+#  merge by the same or opposite alleles to capture all SNPs
+gwas_data_merged <- gwas_data %>%
+  dplyr::left_join(annot %>% select(SNP, CHR, POS, ALLELE0, ALLELE1),
+    by = c("CHR" = "CHR", "POS" = "POS", "ALLELE0" = "ALLELE0", "ALLELE1" = "ALLELE1")
+  ) %>%
+  dplyr::rename(SNP1 = SNP) %>%
+  dplyr::left_join(annot %>% select(SNP, CHR, POS, ALLELE0, ALLELE1),
+    by = c("CHR" = "CHR", "POS" = "POS", "ALLELE0" = "ALLELE1", "ALLELE1" = "ALLELE0")
+  ) %>%
+  dplyr::rename(SNP2 = SNP)
+
+# ----------------------------------------
+# Tidying up and saving
+# ----------------------------------------
+print("Tidying up ..")
+gwas_data_merged <- gwas_data_merged %>%
+  dplyr::select(-ID, -chr_pos) %>%
+  dplyr::mutate(SNP = coalesce(SNP1, SNP2)) %>%
+  dplyr::select(SNP, everything(), -SNP1, -SNP2) %>%
+  dplyr::mutate(SNP = ifelse(is.na(SNP), paste0(CHR, ":", POS), SNP)) %>% # if rs missing, use chr:pos
+  dplyr::distinct()
+print(head(gwas_data_merged))
+
+print("Saving ..")
+vroom::vroom_write(gwas_data2, file = gwas_file_out)
+
+print("Finished adding rsID annotation to your GWAS. Yay!")
