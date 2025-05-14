6c0c85d83c0bc80069f750a5834df9e8e51c216b
Marina Vabistsevits <marina.vabi@hotmail.com>
2025-05-14 12:27:29 +0100

    lifover sctipts initial add

---
+++ 02_liftover_GWAS.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#### Workflow for performing LiftOver of genome build (37<->38) ####
+# to run:
+# ./02_liftover_GWAS.sh /path/to/your/GWAS.txt.gz 38
+
+# Get arguments 
+input_GWAS=$1 # GWAS file (from previous step or separate file);
+              # provide full path; assumes no dots in file name except the ones separating the extension (e.g. ".txt.gz")
+              # GWAS file is expected to be in a standard REGENIE output (see README)
+
+current_build=$2 # starting build; if 38, it will be converted to 37; if 37, it will be converted to 38
+ref_data_dir=$3 # can be refdata/ folder where 00_download_ref_data_liftover.sh saved file or your own location
+
+
+# Separate GWAS file basename and extension
+basename="${input_GWAS%%.*}"
+extension="${input_GWAS#$basename}" 
+
+
+## Perform lifover in the specified direction
+#module load R-bundle-Packages/4.3.2-20240227-gfbf-2023a (for use on Exeter servers only - uncomment)
+
+if [[ "$current_build" == 38 ]]; then 
+    echo "---------- Running R script to perform liftover build 38 -> build 37 ---------- "
+    Rscript liftover_GWAS.R "$current_build" "$ref_data_dir" "$input_GWAS" "$basename"_b37.txt.gz
+
+elif [[ "$current_build" == 37 ]]; then 
+    echo "---------- Running R script to perform liftover build 37 -> build 38 ---------- "
+    Rscript liftover_GWAS.R "$current_build" "$ref_data_dir" "$input_GWAS" "$basename"_b38.txt.gz
+else 
+    echo "No or incorrect build specified; options: 38 or 37"
+fi
---
+++ 02_liftover_helper.R
@@ -0,0 +1,98 @@
+library(rtracklayer)
+library(dplyr)
+library(vroom)
+
+# ----------------------------------------
+# Process arguments
+# ----------------------------------------
+args <- commandArgs(trailingOnly = TRUE)
+input_build <- as.numeric(args[1])
+chain_files <- args[2] # refdata/ folder
+gwas_file_in <- args[3]
+print(paste0("Input GWAS to annotate: ",gwas_file_in))
+gwas_file_out <- args[4]
+print(paste0("Output GWAS with rsIDs: ",gwas_file_out))
+
+# ----------------------------------------
+# Load the required chain file
+# ----------------------------------------
+if (input_build == 38) {
+  chain <- paste0(chain_files, "hg38ToHg19.over.chain") # must be unzipped
+} else if (input_build == 37) {
+  chain <- paste0(chain_files, "hg19ToHg38.over.chain")
+} else {
+  stop("Wrong input build provided; options are 38 or 37")
+}
+
+# ----------------------------------------
+# Read inout GWAS data
+# ----------------------------------------
+paste("Reading data..")
+gwas_file <- vroom(gwas_file_in, 
+                   col_select = c(SNP, CHR, POS, A1FREQ, ALLELE0, ALLELE1, BETA, SE, LOG10P, P)) %>%
+              dplyr::rename(BP = POS)
+
+
+# ----------------------------------------
+# Main function for liftover
+# ----------------------------------------
+liftover_genome <- function(GWAS, path_to_chain) {
+  # based on code from:
+  # https://github.com/RHReynolds/LDSCforRyten/blob/f915728dd8b387f5a1f29abd7bd4684a2b52e4ef/R/GWAS_formatting_functions.R#L11
+  
+  # Import chain file
+  chain_file <- rtracklayer::import.chain(path_to_chain)
+  
+  # If GWAS CHR column does not have "chr" in name, add to allow liftover
+  if (!stringr::str_detect(GWAS$CHR[1], "chr")) {
+    GWAS <- GWAS %>%
+      dplyr::mutate(CHR = stringr::str_c("chr", CHR))
+  }
+  
+  # Convert GWAS to GRanges object
+  GWAS_GR <- GenomicRanges::makeGRangesFromDataFrame(GWAS,
+                                                     keep.extra.columns = TRUE,
+                                                     ignore.strand = TRUE,
+                                                     seqinfo = NULL,
+                                                     seqnames.field = "CHR",
+                                                     start.field = "BP",
+                                                     end.field = "BP",
+                                                     starts.in.df.are.0based = FALSE
+  )
+  
+  GWAS_out <-
+    rtracklayer::liftOver(GWAS_GR, chain_file) %>%
+    unlist() %>%
+    as.data.frame() %>%
+    dplyr::rename(
+      CHR = seqnames,
+      BP = start
+    ) %>%
+    dplyr::select(-end, -width, -strand) %>%
+    dplyr::mutate(CHR = stringr::str_replace(CHR, "chr", ""))
+  
+  return(GWAS_out)
+}
+
+# ----------------------------------------
+# Liftover
+# ----------------------------------------
+paste("Doing liftover..")
+if (input_build == 38) {
+  gwas_file_lifted <- liftover_genome(GWAS = gwas_file, path_to_chain = chain) %>% dplyr::rename(POS = BP)
+} else if (input_build == 37) {
+  gwas_file_lifted <- liftover_genome(GWAS = gwas_file, path_to_chain = chain) %>% dplyr::rename(POS = BP)
+}
+
+# ----------------------------------------
+# Tidy up and save
+# ----------------------------------------
+paste("Updating rsID and saving")
+# for missing rsids use chr:pos
+gwas_file_lifted <- gwas_file_lifted %>%
+  mutate(chr_pos = paste0(CHR, ":", POS)) %>%
+  mutate(SNP = coalesce(SNP, chr_pos)) %>%
+  select(-chr_pos)
+
+vroom_write(gwas_file_lifted, gwas_file_out)
+print("Finished liftover.")
